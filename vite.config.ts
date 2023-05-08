e99084f57a1b6cff8587878819bb1dd6bcd8fc98
xionkq <3030199850@qq.com>
2023-05-08 19:48:50 +0800

    update data fetch

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      "/timeData": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
+  },
 })
