76711061314bd2e2523b4b0c669ab6afee2ea785
xionkq <3030199850@qq.com>
2023-05-26 19:46:25 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,33 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true
+    node: true,
+    'vue/setup-compiler-macros': true,
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'standard-with-typescript'
-  ],
-  overrides: [
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/prettier',
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 2020,
   },
-  plugins: [
-    'vue'
-  ],
   rules: {
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-extra-boolean-cast': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+  globals: {
+    define: true,
+    NodeJS: true,
+  },
 }
